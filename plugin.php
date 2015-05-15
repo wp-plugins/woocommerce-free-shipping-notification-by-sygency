c688b2343583544b53d56a7804970d1a0052dc2e
Sygency <Sygency@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 11:00:57 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-free-shipping-notification-by-sygency/trunk@1161076 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Woocommerce Free Shipping Notification by Sygency
+Plugin URI: https://github.com/sygency/woocommerce-free-shipping-notification
+Description: This plugin shows how much money user should spend in order to get free shipping if available.
+Version: 1.1
+Author: UAB Synergy Technologies
+Author URI: http://www.sygency.com
+Requires at least: 3.7
+Tested up to: 4.2.2
+License: GPL2
+*/
+
+// block direct access to plugin file
+defined('ABSPATH') or die( __("No script kiddies please!", 'tinyrelated' ) );
+
+/**
+ * Cart Notice of Free Shipping
+ *
+ */
+function cart_notice() {
+    global $wpdb, $woocommerce;
+
+    $mydbselect = $wpdb->get_var( "SELECT option_value FROM  ".$wpdb->prefix."options WHERE option_name = 'woocommerce_free_shipping_settings'" );
+    $array_temp = unserialize ($mydbselect);
+    $maximum = $array_temp['min_amount'];
+    $current = WC()->cart->subtotal;
+    if ( $current < $maximum ) {
+        echo '<div class="woocommerce-message">' . __('You need to add $', 'syg'). ($maximum - $current) .__(' more to your cart, in order to use free shipping.', 'syg'). '</div>';
+    }
+}
+add_action( 'woocommerce_before_cart', 'cart_notice' );
+
+
+/**
+ * Mini Cart Notice of Free Shipping
+ *
+ */
+if ( ! function_exists( 'woocommerce_mini_cart' ) ) {
+
+    function woocommerce_mini_cart( $args = array() ) {
+
+        global $wpdb, $woocommerce;
+        // Select Data From Database
+        $mydbselect = $wpdb->get_var( "SELECT option_value FROM  ".$wpdb->prefix."options WHERE option_name = 'woocommerce_free_shipping_settings'" );
+        $array_temp = unserialize ($mydbselect);
+        // Get Amount from input in WooCommerce > Shipping > Free Shipping
+        $maximum = $array_temp['min_amount'];
+        $current = WC()->cart->subtotal;
+        // Display Message
+        if ( $current < $maximum ) {
+            echo '<div class="woocommerce-message">' . __('You need to add  $', 'syg'). ($maximum - $current) .__(' more to your cart, in order to use free shipping.', 'syg'). '</div>';
+        }
+
+        // Show Default Cart View
+        $defaults = array( 'list_class' => '' );
+        $args = wp_parse_args( $args, $defaults );
+        wc_get_template( 'cart/mini-cart.php', $args );
+
+    }
+
+}
